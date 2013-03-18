2dc1b1ab1497d8b2f96db6e60ef060c772e4563c
kevin.xu <kevin.xu@gree.co.jp>
2013-03-18 16:13:04 +0800

    auto flush

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Cardsys::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
+  config.logger.auto_flushing = true
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
