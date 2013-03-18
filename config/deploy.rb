290ac588822011e7b76e4f44a4d7cfd22d7083f1
cming <cming.xu@photomugs.com>
2013-03-18 22:26:12 +0800

    database.template change

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ after "deploy:update_code", "deploy:update_bundle"
 
 namespace :db do
   task :db_config, :except => { :no_release => true }, :role => :app do
-    run "cp -f ~/database.template #{release_path}/config/database.yml"
+    run "cp -f #{release_path}/database.template #{release_path}/config/database.yml"
   end
 end
 
