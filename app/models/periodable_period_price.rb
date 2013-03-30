8c9b34517c23694d54ae7d944c3ea1fad48bb9de
neocanable <neocanable@gmail.com>
2013-03-31 00:54:16 +0800

    attr_accessiable

--- app/models/periodable_period_price.rb
+++ app/models/periodable_period_price.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class PeriodablePeriodPrice < ActiveRecord::Base
-  attr_accessible :price
+  attr_accessible :price, :period_price_id
   belongs_to :periodable, :polymorphic => true
   belongs_to :period_price
 end
