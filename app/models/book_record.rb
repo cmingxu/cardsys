c6bbc719499a4da1ec8527325e7abdb4b35c469b
cming.xu <cming.xu@gmail.com>
2013-04-07 20:42:33 +0800

    remove unused methods

--- app/models/book_record.rb
+++ app/models/book_record.rb
@@ -1,21 +1,5 @@
 # -*- encoding : utf-8 -*-
 class BookRecord < ActiveRecord::Base
-  Status_Default     = 0  #等待与等
-  Status_Prearranged = 1  #已预定
-  Status_Settling    = 2  #已结算
-  Status_cancel      = 3  #已取消
-  Status_Agent       = 11 #代买中
-  Status_Active      = 12 #打开中
-  Status_Do_Agent    = 13
-
-  All_Operations = [:book,:agent,:active,:balance,:cancel,:do_agent,:change_coaches]
-  OPERATION_MAP = {:book => "预定",
-    :agent => "申请代卖",
-    :do_agent => "代卖",
-    :balance => "结算",
-    :active => "开场",
-    :cancel => "取消预定",
-    :change_coaches => "变更教练"}
 
   belongs_to  :order
   belongs_to  :resource, :polymorphic => true
@@ -44,108 +28,8 @@ class BookRecord < ActiveRecord::Base
     resource.try(:name)
   end
 
-  def agent_to_buy_condition
-    CommonResource.agent_to_buy_time.hours.ago(self.start_date_time) >= DateTime.now 
-  end
-
-  def cancel_condition
-    CommonResource.cancel_time.hours.ago(self.start_date_time) >= DateTime.now
-  end
-
-  def change_conditions
-    CommonResource.change_time.hours.ago(self.start_date_time) > DateTime.now
-  end
-
-  #开场前半小时到结束时段
-  def active_conditions
-    self.alloc_date.beginning_of_day == Time.now.beginning_of_day
-  end
-
-  def should_book?
-    is_default? || should_to_agent_to_buy? || is_to_do_agent?
-  end
-
-  def should_to_agent_to_buy?
-    is_agented? && agent_to_buy_condition
-  end
-
-  def should_application_to_agent?
-    is_booked? && agent_to_buy_condition 
-  end
-
-  def should_to_cancel?
-    is_booked? && cancel_condition
-  end
-
-  def should_changed?
-    is_booked? && change_conditions
-  end
-
-  def should_to_active?
-    (is_booked? || is_agented?) && active_conditions
-  end
-
-  #TODO
-  def should_to_balance?
-    is_actived?
-  end
-
-  def should_blance_as_expired?
-    # is_agented 包含了时间条件判断
-    #(is_booked? || is_agented?) && is_expired?
-    (is_booked? || status == Status_Agent) && is_expired?
-  end
-
-
-  def status_desc
-    desc = case
-           when  booked?      then   expired? ? "已预定(过期)" : "已预定"
-           when  balanced?    then   "已结算"
-           when  canceld?     then   "已取消"
-           when  agented?     then
-             expired? ? "停止代买" : "代卖中"
-           when  actived?      then   "开打中"
-           else "过期"
-           end
-    desc
-  end
-
-  def status_color
-    color = case
-            when  is_default?      then  ""
-            when  is_booked?         then  "color01"
-            when  is_balanced?       then  "color05"
-            when  is_canceld?        then  ""
-            when  is_agented?        then  "color04"
-            when  is_actived?        then  "color03"
-            when  status == Status_Agent        then  "color04"
-            else "color02"
-            end
-    color
-  end
-
   def hours
     end_hour - start_hour
-  end
-
-  def amount
-    order.is_member? ? amount_by_card : amount_by_court
-  end
-
-  def amount_desc(order_item = nil)
-    (order.is_member? && order.member_card.card.is_counter_card?) ?  "#{amount}次" : "￥#{amount}"
-  end
-
-  def amount_by_court
-    court.calculate_amount_in_time_span(alloc_date,start_hour,end_hour)
-  end
-
-  def amount_by_card
-    order.members_card.calculate_amount_in_time_span(alloc_date,start_hour,end_hour)
-  end
-
-  def consecutive?
-    self.order.is_advanced_order?
   end
 
 
