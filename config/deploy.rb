ea9ba7d9237f88724e9ccd3f44765746fe5378e2
cming.xu <cming.xu@gmail.com>
2013-04-28 22:58:25 +0800

    rvm-capistrno

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,7 @@ namespace :deploy do
   end
 end
 
-after "deploy:update_code", "deploy:update_bundle"
+#after "deploy:update_code", "deploy:update_bundle"
 
 namespace :db do
   task :db_config, :except => { :no_release => true }, :role => :app do
